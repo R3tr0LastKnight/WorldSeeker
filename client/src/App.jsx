abdf31d06972b0b5bee9aba0502f12ba2b5db260
R3tr0LastKnight <shubh14shahu@gmail.com>
2023-11-18 01:28:47 +0530

    debug

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -13,7 +13,7 @@ import PlacePage from "./pages/PlacePage";
 import BookingsPage from "./pages/BookingsPage";
 import BookingPage from "./pages/BookingPage";
 
-axios.defaults.baseURL = "http://localhost:4000";
+axios.defaults.baseURL = process.env.API_BASE_URL;
 // axios.defaults.baseURL = "https://worldseekerbackend.onrender.com";
 axios.defaults.withCredentials = true;
 
